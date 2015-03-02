4cfe634c42c850795c3b843706273acc79f066d9
Margaux Divernois <margaux.divernois@gmail.com>
2015-03-02 17:47:21 +0100

    Push des Equations Différentielles

--- Playground/src/margaux/EquationDifferentielle.java
+++ Playground/src/margaux/EquationDifferentielle.java
@@ -10,8 +10,9 @@ public class EquationDifferentielle
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
 
-	public EquationDifferentielle(double content, double content_val, double in_val, double in_debit, double out_debit)
+	public EquationDifferentielle(double capacite, double content, double content_val, double in_val, double in_debit, double out_debit)
 		{
+		this.capacite = capacite;
 		this.content = content;
 		this.content_val = content_val;
 		this.in_val = in_val;
@@ -34,6 +35,27 @@ public class EquationDifferentielle
 	public double tempsQuantiteQ(double q)
 		{
 			return Math.log((q - Math.pow(out,-1)*in ) / C) / (-out);
+		}
+
+	public double tempsQuantitePourcent(double qPourcent)
+		{
+			//return Math.log((q - Math.pow(out,-1)*in ) / C) / (-out);
+			return 0;
+		}
+
+	public double tempsDebordement()
+		{
+			return (capacite - content)/(in_debit - out_debit);
+		}
+
+	public double tempsVide()
+		{
+			return (- content)/(in_debit - out_debit);
+		}
+
+	public double quantiteDebordement()
+		{
+			return quantiteTempsT(tempsDebordement());
 		}
 
 	private void init()
@@ -66,6 +88,7 @@ public class EquationDifferentielle
 	\*------------------------------------------------------------------*/
 
 	//Input
+	private double capacite;
 	private double content;
 	private double content_val;
 	private double in_val;
--- Playground/src/margaux/UseEquationDifferentielle.java
+++ Playground/src/margaux/UseEquationDifferentielle.java
@@ -18,12 +18,19 @@ public class UseEquationDifferentielle
 
 	public static void main()
 		{
-			EquationDifferentielle equadiff = new EquationDifferentielle(50, 25, 4, 2, 2);
-			double y = equadiff.quantiteTempsT(25);
-			double t = equadiff.tempsQuantiteQ(135.62109779499758);
+			EquationDifferentielle equadiff = new EquationDifferentielle(1000, 300, 50, 0, 10, 40);
+//			double y = equadiff.quantiteTempsT(25);
+//			double t = equadiff.tempsQuantiteQ(135.62109779499758);
 //			equadiff.getEquation();
 
+			double y = equadiff.quantiteDebordement();
+			double t = equadiff.tempsDebordement();
+
+			double tVide = equadiff.tempsVide();
+
 			System.out.println(y+" "+t);
+
+			System.out.println(tVide);
 		}
 
 	/*------------------------------------------------------------------*\
