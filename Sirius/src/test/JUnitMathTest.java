dbfb65ed2a2990274ef9873821969cbfb2465aa5
Margaux Divernois <margaux.divernois@gmail.com>
2015-05-30 10:49:29 +0200

    Correction orthographique sur JPanelStopQuantityQ Ajout des méthodes "isEquals" dans MathTools Ajout d'une classe de test JUNIT qui vérifie la partie mathématique.

---
+++ Sirius/src/test/JUnitMathTest.java
@@ -0,0 +1,103 @@
+package test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import substance.Substance;
+import tank.Tank;
+import tools.MathTools;
+
+public class JUnitMathTest
+{
+/*------------------------------------------------------------------*\
+	 |*							Constructeurs							*|
+	 \*------------------------------------------------------------------*/
+
+	private static Substance eau;
+	private static Substance sel;
+
+@Before public void before()
+	{
+	eau = new Substance("Eau", (float)0.6, Substance.LIQUID);
+	sel = new Substance("Sel", (float)1.0, Substance.SOLID);
+	}
+
+@After public void after()
+	{
+	// rien
+	}
+
+/*------------------------------------------------------------------*\
+	 |*							Methodes Public							*|
+	 \*-----------------------------------------------------------------*/
+
+@Test public void testDidierMuller()
+	{
+	// http://www.apprendre-en-ligne.net/MADIMU2/ANALY/ANALY9.PDF
+
+	Tank r1 = new Tank(false, 500, 5);
+	r1.setName("ParentTank");
+	Tank r2 = new Tank(false, 200, 5);
+	r2.setName("MainTank");
+
+	r1.addSubstance(sel, 2);
+	r1.addSubstance(eau, 500);
+
+	r2.addSubstance(sel, 4);
+	r2.addSubstance(eau, 100);
+	r2.addTankParent(r1);
+
+	double valeurDuProgramme = r2.equaDiff(10,sel);
+	double valeurAttendue = 81.1;
+
+	Assert.assertTrue(MathTools.isEquals(valeurDuProgramme, valeurAttendue, 0.01));
+	}
+
+@Test public void testSerie4Ex1()
+	{
+	Tank r1 = new Tank(false, 5000000, 25);
+	r1.addSubstance(sel, 20);
+	r1.addSubstance(eau, 5000);
+
+	Tank r2 = new Tank(false, 200, 25);
+	r2.addSubstance(sel, 0.03);
+	r2.addSubstance(eau, 100);
+
+	r1.addTankParent(r2);
+
+	double valeurDuProgramme = r1.equaDiff(30,sel);
+	double valeurAttendue = 38.1;
+
+	Assert.assertTrue(MathTools.isEquals(valeurDuProgramme, valeurAttendue, 0.01));
+	}
+
+@Test public void testSerie5Ex2()
+	{
+	Tank r0 = new Tank(false, 5000, 20);
+	r0.addSubstance(sel,0);
+	r0.addSubstance(eau, 5000);
+
+	Tank r1 = new Tank(false, 500, 20);
+	r1.addSubstance(sel, 1);
+	r1.addSubstance(eau, 200);
+
+	r1.addTankParent(r0);
+
+	double t;
+
+	t = Math.random();
+
+	double valeurDuProgramme = r1.equaDiff(t,sel);
+	double valeurAttendue = Math.exp(-0.1*t);
+
+	//Point a de l'exercice
+	Assert.assertTrue(MathTools.isEquals(valeurDuProgramme, valeurAttendue, 0.0001));
+	}
+
+/*------------------------------------------------------------------*\
+	|*							Methodes Private						*|
+	\*------------------------------------------------------------------*/
+
+}
--- Sirius/src/tools/MathTools.java
+++ Sirius/src/tools/MathTools.java
@@ -11,5 +11,28 @@ public class MathTools
 	    return b == 0 ? a : gcm(b, a % b);
 	}
 
+	/**
+	 * Exemple: <b>epsilon</b> = 1E-6
+	 */
+	public static boolean isEquals(double a, double b, double epsilon)
+		{
+		if (a == 0 || b == 0)
+			{
+			return Math.abs(a - b) <= epsilon;
+			}
+		else
+			{
+			return Math.abs((a - b) / a) <= epsilon;
+			}
+		}
+
+	/**
+	 * Exemple: <b>epsilon</b> = 1E-6
+	 */
+	public static boolean isEquals(long a, long b, long epsilon)
+		{
+		return Math.abs(a-b) <= epsilon;
+		}
+
 	}
 
